58432135e83ab7dfaa35baaea887ac882c5aa04c
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-12-16 14:34:50 +0200

    feat(scripts): add symlinks

---
+++ general/scripts/molecule-dep-symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $PWD/roles/nodejs $PWD/collections/ansible_collections/oswee/general/roles/nodejs
+ln -s $PWD/roles/yarn $PWD/collections/ansible_collections/oswee/general/roles/yarn
+ln -s $PWD/roles/github $PWD/collections/ansible_collections/oswee/general/roles/github
+ln -s $PWD/roles/nvim $PWD/collections/ansible_collections/oswee/general/roles/nvim
+
